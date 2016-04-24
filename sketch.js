61bff3d1024a31c6eab71a66ed8c6b21984b06ad
berenger <berenger.recoules@gmail.com>
2016-04-24 18:48:09 +0200

    tweaks and remove dead code

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
-// fix cam z position control with dat-gui
-// fix remove boid function (actually implement it !)
-// remove more boids at a time
-// detect orientation of each boid
-
 
 
 var flock;
 
-var fov = 0;
-var cameraZ = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight,WEBGL);
 
   flock = new Flock();
   // Add an initial set of boids into the system
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 150; i++) {
     var b = new Boid(1,1,250);
     flock.addBoid(b);
   }
@@ -24,13 +17,6 @@ function setup() {
   cc = new camControls();
   gui = new dat.GUI();
   initGui();
-
-
-  var v1 = createVector(1, 0, 0);
-  var v2 = createVector(0, 1, 0);
- 
-  var angle = p5.Vector.angleBetween(v1, v2);
-  console.log(angle);
 }
 
 function draw() {
@@ -297,9 +283,10 @@ var initGui = function() {
   f2.add(sp, 'cohesion' ,-10,10);  
   f2.add(sp, 'add_boid');
   f2.add(sp, 'remove_boid');  
+  f2.add(sp, 'reset_flock');
 
   var f = gui.addFolder('Camera controls');
-  f.add(cc, 'z_position', -800,800);
+  f.add(cc, 'z_position', -1500,1500);
   f.add(cc, 'x_rotation', -PI/2,PI/2);
   f.add(cc, 'y_rotation', -PI/2,PI/2);
   f.add(cc, 'reset_camera');
@@ -312,7 +299,18 @@ var simulationParameters = function(){
 
     this.add_boid = function (){flock.addBoid(new Boid(random(windowWidth),random(windowHeight)))}
     this.remove_boid = function (){flock.removeBoid();}
+
+    this.reset_flock = function (){
+      flock = new Flock();
+      for (var i = 0; i < 150; i++) {
+        var b = new Boid(1,1,250);
+        flock.addBoid(b);
+      }
+    }
 }
+
+
+
 
 var camControls = function(){
   this.z_position = 0;
@@ -324,108 +322,4 @@ var camControls = function(){
      this.x_rotation = 0;
      this.y_rotation = 0;
   }
-}
-
-
-function makePerspective(fieldOfViewInRadians, aspect, near, far) {
-  var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
-  var rangeInv = 1.0 / (near - far);
-  return [
-    f / aspect, 0, 0, 0,
-    0, f, 0, 0,
-    0, 0, (near + far) * rangeInv, -1,
-    0, 0, near * far * rangeInv * 2, 0
-  ];
-};
-
-function makeTranslation(tx, ty, tz) {
-  return [
-     1,  0,  0,  0,
-     0,  1,  0,  0,
-     0,  0,  1,  0,
-    tx, ty, tz,  1
-  ];
-}
-function makeXRotation(angleInRadians) {
-  var c = Math.cos(angleInRadians);
-  var s = Math.sin(angleInRadians);
-  return [
-    1, 0, 0, 0,
-    0, c, s, 0,
-    0, -s, c, 0,
-    0, 0, 0, 1
-  ];
-};
-function makeYRotation(angleInRadians) {
-  var c = Math.cos(angleInRadians);
-  var s = Math.sin(angleInRadians);
-  return [
-    c, 0, -s, 0,
-    0, 1, 0, 0,
-    s, 0, c, 0,
-    0, 0, 0, 1
-  ];
-};
-function makeZRotation(angleInRadians) {
-  var c = Math.cos(angleInRadians);
-  var s = Math.sin(angleInRadians);
-  return [
-     c, s, 0, 0,
-    -s, c, 0, 0,
-     0, 0, 1, 0,
-     0, 0, 0, 1,
-  ];
-}
-
-
-
-function matrixMultiply(a, b) {
-  var a00 = a[0*4+0];
-  var a01 = a[0*4+1];
-  var a02 = a[0*4+2];
-  var a03 = a[0*4+3];
-  var a10 = a[1*4+0];
-  var a11 = a[1*4+1];
-  var a12 = a[1*4+2];
-  var a13 = a[1*4+3];
-  var a20 = a[2*4+0];
-  var a21 = a[2*4+1];
-  var a22 = a[2*4+2];
-  var a23 = a[2*4+3];
-  var a30 = a[3*4+0];
-  var a31 = a[3*4+1];
-  var a32 = a[3*4+2];
-  var a33 = a[3*4+3];
-  var b00 = b[0*4+0];
-  var b01 = b[0*4+1];
-  var b02 = b[0*4+2];
-  var b03 = b[0*4+3];
-  var b10 = b[1*4+0];
-  var b11 = b[1*4+1];
-  var b12 = b[1*4+2];
-  var b13 = b[1*4+3];
-  var b20 = b[2*4+0];
-  var b21 = b[2*4+1];
-  var b22 = b[2*4+2];
-  var b23 = b[2*4+3];
-  var b30 = b[3*4+0];
-  var b31 = b[3*4+1];
-  var b32 = b[3*4+2];
-  var b33 = b[3*4+3];
-  return [a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30,
-          a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31,
-          a00 * b02 + a01 * b12 + a02 * b22 + a03 * b32,
-          a00 * b03 + a01 * b13 + a02 * b23 + a03 * b33,
-          a10 * b00 + a11 * b10 + a12 * b20 + a13 * b30,
-          a10 * b01 + a11 * b11 + a12 * b21 + a13 * b31,
-          a10 * b02 + a11 * b12 + a12 * b22 + a13 * b32,
-          a10 * b03 + a11 * b13 + a12 * b23 + a13 * b33,
-          a20 * b00 + a21 * b10 + a22 * b20 + a23 * b30,
-          a20 * b01 + a21 * b11 + a22 * b21 + a23 * b31,
-          a20 * b02 + a21 * b12 + a22 * b22 + a23 * b32,
-          a20 * b03 + a21 * b13 + a22 * b23 + a23 * b33,
-          a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30,
-          a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31,
-          a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32,
-          a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33];
 }
